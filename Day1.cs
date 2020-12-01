a6a2f73fb97076ed1dbdaaa96715e00f886bebc7
Ede Meijer <ede@elastict.nl>
2020-12-01 10:39:45 +0100

    Day 1 part 2

--- Day1.cs
+++ Day1.cs
@@ -6,17 +6,35 @@ namespace aoc2020
 {
     internal static class Day1
     {
-        internal static void Run()
+        internal static void Part1()
         {
             var input = Input.Read(1);
             var numbers = input.Select(int.Parse).ToList();
 
             foreach (var number in numbers)
             {
-                foreach (var number2 in numbers.Where(number2 => number + number2 == 2020))
+                foreach (var number2 in numbers.Where(n => number + n == 2020))
                 {
                     Console.WriteLine(number * number2);
                     return;
+                }
+            }
+        }
+        
+        internal static void Part2()
+        {
+            var input = Input.Read(1);
+            var numbers = input.Select(int.Parse).ToList();
+
+            foreach (var number in numbers)
+            {
+                foreach (var number2 in numbers.Where(n => number + n < 2020))
+                {
+                    foreach (var number3 in numbers.Where(n => number + number2 + n == 2020))
+                    {
+                        Console.WriteLine(number * number2 * number3);
+                        return;
+                    }
                 }
             }
         }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         private static void Main(string[] args)
         {
-            Day1.Run();
+            // Day1.Part1();
+            Day1.Part2();
         }
     }
 }
