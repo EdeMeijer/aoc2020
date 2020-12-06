d9d64c4d15a956571d5f989645149225352d6825
Ede Meijer <ede@elastict.nl>
2020-12-06 09:49:03 +0100

    Day 6 part 2

--- Src/Day6.cs
+++ Src/Day6.cs
@@ -13,13 +13,25 @@ namespace Aoc2020
             var result = input
                 .Split("\n\n")
                 .Sum(group => group.ToCharArray().Where(c => c != '\n').ToHashSet().Count);
-            
+
             Console.WriteLine(result);
         }
 
         internal static void Part2()
         {
             var input = Input.Text(6);
+
+            var result = input
+                .Split("\n\n")
+                .Sum(group =>
+                {
+                    var groupSize = group.Split('\n').Length;
+                    var answers = group.ToCharArray().Where(c => c != '\n').ToList();
+                    return answers.ToHashSet()
+                        .Count(uniqAnswer => answers.Count(a => a == uniqAnswer) == groupSize);
+                });
+
+            Console.WriteLine(result);
         }
     }
 }
